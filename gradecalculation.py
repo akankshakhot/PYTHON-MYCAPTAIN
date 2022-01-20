2dca9ce7fea5a4b0dc949dc0e041d09bb5a21e99
akankshakhot <92516099+akankshakhot@users.noreply.github.com>
2022-01-20 16:03:29 +0530

    Add files via upload

---
+++ gradecalculation.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 20 15:48:00 2022
+
+@author: hp
+"""
+
+print("enter your marks for five subjects below")
+English=int(input("enter your marks of english:"))
+History=int(input("enter your marks of history:"))
+maths=int(input("enter your marks of maths:"))
+science=int(input("enter your marks of science:"))
+geography=int(input("enter your marks of geography:"))
+
+total = (English+History+maths+science+geography)/5
+avg= total/5
+
+
+if avg>=91 and avg<=100:
+    print("Your Grade is A1")
+elif avg>=81 and avg<91:
+    print("Your Grade is A2")
+elif avg>=71 and avg<81:
+    print("Your Grade is B1")
+elif avg>=61 and avg<71:
+    print("Your Grade is B2")
+elif avg>=51 and avg<61:
+    print("Your Grade is C1")
+elif avg>=41 and avg<51:
+    print("Your Grade is C2")
+elif avg>=33 and avg<41:
+    print("Your Grade is D")
+elif avg>=21 and avg<33:
+    print("Your Grade is E1")
+elif avg>=0 and avg<21:
+    print("Your Grade is E2")
+else:
+    print("Invalid Input!")
